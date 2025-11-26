79239821a390d3364b95b354d1f05b7075a005af
Syahrizal Khairan <13523063@std.stei.itb.ac.id>
2025-11-26 10:28:30 +0700

    fix: play music on start from main menu

--- Assets/Scripts/UI Menu/MainMenu.cs
+++ Assets/Scripts/UI Menu/MainMenu.cs
@@ -7,10 +7,9 @@ public class MainMenu : MonoBehaviour
    {
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1);
 
-        // Pilihan lainnya taruh event listener di asyncLoad daripada busy waiting
+        // taruh event listener di asyncLoad daripada busy waiting
         // tapi by nature of async bisa aja listener ditambahin setelah load selesai idk ga tau tech Unity
-        while (!asyncLoad.isDone) {}
-        BGMManager.Instance?.PlayMusic();
+        asyncLoad.completed += (AsyncOperation op) => BGMManager.Instance?.PlayMusic();
    }
    public void QuitGame()
     {
