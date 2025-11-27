f00152a8ae2acc5e0b5b4834834c4a6318ef08f3
Rizal <syahrizal.khairan@gmail.com>
2025-11-27 20:29:35 +0700

    feat: event item chunk generation

--- Assets/Scripts/ChunkScript/ChunkGenerator.cs
+++ Assets/Scripts/ChunkScript/ChunkGenerator.cs
@@ -5,6 +5,12 @@ public class ChunkGenerator : MonoBehaviour
 {
     [Header("Daftar Semua Chunk")]
     public List<GameObject> chunkPrefabs; 
+    public List<GameObject> eventChunkPrefabs;
+
+    [Header("Semua Item Event Object")]
+    public List<GameObject> eventItems;
+    
+    public Transform eventItemSpawnPoint;
 
     [Header("Posisi Awal")]
     public Transform startPoint; 
@@ -16,6 +22,8 @@ public class ChunkGenerator : MonoBehaviour
     // Variabel internal
     private Transform currentExitPoint; 
     public float spawnTriggerX = 0f;  
+
+    private int eventCounter = 0;// Sementara setiap 5 chunk
 
     void Start()
     {
@@ -32,15 +40,23 @@ public class ChunkGenerator : MonoBehaviour
     {
         if (currentExitPoint != null && currentExitPoint.position.x < spawnTriggerX)
         {
-            SpawnRandomChunk();
+            eventCounter++;
+            Debug.Log("Event Counter: " + eventCounter);
+            if (eventCounter > 5) eventCounter = 0;
+            SpawnRandomChunk(eventCounter == 5);
         }
     }
 
-    void SpawnRandomChunk()
+    void SpawnRandomChunk(bool eventChunk = false)
     {
         if (chunkPrefabs.Count == 0) return; 
         
         GameObject prefabToSpawn = chunkPrefabs[Random.Range(0, chunkPrefabs.Count)];
+        if (eventChunk && eventChunkPrefabs.Count > 0)
+        {
+            Debug.Log("Spawning Event Chunk");
+            prefabToSpawn = eventChunkPrefabs[Random.Range(0, eventChunkPrefabs.Count)];
+        }
 
         // --- BAGIAN PENGATUR JARAK ---
         
@@ -57,6 +73,19 @@ public class ChunkGenerator : MonoBehaviour
         // -----------------------------
 
         GameObject newChunk = Instantiate(prefabToSpawn, finalSpawnPos, Quaternion.identity);
+
+        Transform eventItemPoint = newChunk.transform.Find("EventItemSpawn");
+            
+        if (eventChunk && eventItemPoint != null && eventItems.Count > 0)
+        {
+            Debug.Log("Spawning Event Item");
+            GameObject eventItemPrefab = eventItems[Random.Range(0, eventItems.Count)];
+            if (eventItemPrefab == null)
+            {
+                Debug.LogError("Event item prefab is NULL or destroyed!");
+            }
+            Instantiate(eventItemPrefab, eventItemPoint.position, Quaternion.identity, newChunk.transform);
+        }
 
         ChunkExitPoint newExit = newChunk.GetComponentInChildren<ChunkExitPoint>();
 
